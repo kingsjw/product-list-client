6fe761e7ac65f8d427c2f07913aec3f5d01fcf04
kingsjw <kingsjw7@gmail.com>
2021-03-24 18:20:09 +0900

    Implement Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { theme } from './assets/styles/theme'
 import { ThemeProvider } from 'styled-components';
+import Header from './components/common/Header';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
+        <Header/>
         <Switch>
           <Route path='/'>
             <div className="App"></div>
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+const HeaderComponent = () => {
+  return (
+    <Header>
+      <Wrap>
+        <Container>
+          <Logo>kingsjw #</Logo>
+          <Menu>
+            <MenuItem>
+              <NavLink
+                to='/products'
+                activeClassName='active'
+              >상품목록</NavLink>
+            </MenuItem>
+            <MenuItem>
+              <NavLink
+                to='/cart'
+                activeClassName='active'
+              >장바구니</NavLink>
+            </MenuItem>
+          </Menu>
+        </Container>
+      </Wrap>
+    </Header>
+  );
+};
+
+const Header = styled.header`
+  width: 100%;
+  height: 80px;
+`;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid #f2f2f2;
+  position: fixed;
+  top: 0;
+`;
+
+const Container = styled.div`
+  width: ${ props => props.theme.wrapWidth };
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Logo = styled.div`
+  font-size: 22px;
+  font-weight: bold;
+  color: #000;
+  cursor: pointer;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+`;
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  margin-left: 27px;
+  &:first-of-type{
+    margin-left: 0;
+  }
+  a{
+    display: block;
+    color: inherit;
+    text-decoration: none;
+    color: #000;
+    &.active{
+      color: ${ props => props.theme.color.main };
+    }
+    &:hover{
+      color: ${ props => props.theme.color.main };
+    }
+  }
+`;
+
+export default HeaderComponent;
