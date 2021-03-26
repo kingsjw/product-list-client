96a65e5162b11e85a94db115681c1a96a1d7ea3b
kingsjw <kingsjw7@gmail.com>
2021-03-26 21:38:19 +0900

    Add dummy api of product data pagination api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,12 @@
 import productListData from '../data/products';
 
+const storageKey = 'class101-cart';
+const getStorageData = () => JSON.parse(JSON.stringify(window.localStorage.getItem(storageKey) || []));
+
 export const getRecommendProductApi = () => {
   const randomIndexList: Array<number> = [];
   while (randomIndexList.length < 4) {
-    const randomIndex = Math.floor(Math.random() * ((productListData.length + 0))) + 0;
+    const randomIndex = Math.floor(Math.random() * productListData.length);
     if (randomIndexList.indexOf(randomIndex) === -1) {
       randomIndexList.push(randomIndex);
     }
@@ -13,5 +16,22 @@ export const getRecommendProductApi = () => {
     setTimeout(() => {
       resolve({ data: result });
     }, 1500);
+  });
+};
+
+
+export const getProductDataPaginationApi = ({ page }: { page: number }) => {
+  return new Promise((resolve, reject) => {
+    const CONTENT_COUNT = 6;
+    const startPage = (page - 1) * CONTENT_COUNT;
+    const cartStorage = getStorageData();
+    setTimeout(() => {
+      resolve({
+        data: {
+          contents: productListData.map((obj) => ({ ...obj, isCarted: cartStorage.indexOf(obj.id) !== -1 })).slice(startPage, startPage + CONTENT_COUNT),
+          totalPage: Math.ceil(productListData.length / CONTENT_COUNT),
+        }
+      });
+    }, 1000)
   });
 };
