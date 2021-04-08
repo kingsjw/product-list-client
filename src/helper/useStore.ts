2445855135430cb1270b30a2434697f67e5d1ac2
kingsjw <kingsjw7@gmail.com>
2021-04-08 18:39:35 +0900

    (Remove): mobx, dummy api

--- src/helper/useStore.ts
+++
@@ -1,6 +0,0 @@
-import { productStore } from '../store/product.store';
-import { homeStore } from '../store/home.store';
-
-const useStore = () => ({ productStore, homeStore });
-
-export default useStore;
